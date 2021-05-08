a1c0b3105c18130a6cad48f0f3dc632ad9a606c1
Nes B <anes.bkdm@live.fr>
2021-05-08 06:11:59 +0200

    Added IERC721 code

--- contracts/IERC721.sol
+++ contracts/IERC721.sol
@@ -20,17 +20,17 @@ interface IERC721 {
      */
     function balanceOf(address owner) external view returns (uint256 balance);
 
-    /*
+    /**
      * @dev Returns the total number of tokens in circulation.
      */
     function totalSupply() external view returns (uint256 total);
 
-    /*
+    /**
      * @dev Returns the name of the token.
      */
     function name() external view returns (string memory tokenName);
 
-    /*
+    /**
      * @dev Returns the symbol of the token.
      */
     function symbol() external view returns (string memory tokenSymbol);
@@ -45,7 +45,7 @@ interface IERC721 {
     function ownerOf(uint256 tokenId) external view returns (address owner);
 
 
-     /* @dev Transfers `tokenId` token from `msg.sender` to `to`.
+    /** @dev Transfers `tokenId` token from `msg.sender` to `to`.
      *
      *
      * Requirements:
@@ -57,4 +57,41 @@ interface IERC721 {
      * Emits a {Transfer} event.
      */
     function transfer(address to, uint256 tokenId) external;
+
+
+    /******************/
+    /*    8 5 2021    */
+    /******************/
+
+    /** @notice Change or reaffirm the approved address for an NFT
+     * @dev The zero address indicates there is no approved address.
+     *  Throws unless `msg.sender` is the current NFT owner, or an authorized
+     *  operator of the current owner.
+     * @param _approved The new approved NFT controller
+     * @param _tokenId The NFT to approve
+     */
+    function approve(address _approved, uint256 _tokenId) external;
+
+    /** @notice Enable or disable approval for a third party ("operator") to manage
+     *  all of `msg.sender`'s assets
+     * @dev Emits the ApprovalForAll event. The contract MUST allow
+     *  multiple operators per owner.
+     * @param _operator Address to add to the set of authorized operators
+     * @param _approved True if the operator is approved, false to revoke approval
+     */
+    function setApprovalForAll(address _operator, bool _approved) external;
+
+    /** @notice Get the approved address for a single NFT
+     * @dev Throws if `_tokenId` is not a valid NFT.
+     * @param _tokenId The NFT to find the approved address for
+     * @return The approved address for this NFT, or the zero address if there is none
+     */
+    function getApproved(uint256 _tokenId) external view returns (address);
+
+    /** @notice Query if an address is an authorized operator for another address
+     * @param _owner The address that owns the NFTs
+     * @param _operator The address that acts on behalf of the owner
+     * @return True if `_operator` is an approved operator for `_owner`, false otherwise
+     */
+    function isApprovedForAll(address _owner, address _operator) external view returns (bool);
 }
